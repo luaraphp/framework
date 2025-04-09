62fbc3014858f61f55231ca04b232be3ed08a5a6
Amine Amazou <amazou@outlook.fr>
2025-04-09 22:54:28 +0100

    feat(Form): create the form() helper to simplify rendering a form.

--- src/Effortless/Support/helpers.php
+++ src/Effortless/Support/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
     use Effortless\View\View;
+    use Effortless\Support\Form;
 
     if(! function_exists('view')) {
 
@@ -9,6 +10,14 @@
                 ->setPath()
                 ->compile()
                 ->render();
+        }
+
+    }
+
+    if(! function_exists('form')) {
+
+        function form($method = "GET", $action = "", $fields = []) {
+            return (new Form($method, $action))->render();
         }
 
     }
